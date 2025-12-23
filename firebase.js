8fed90b3b63d2d6d241e18e7550e44e056e69e58
laculturaltelefonica-ship-it <laculturaltelefonica@gmail.com>
2025-12-23 10:42:41 +0100

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -4,13 +4,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY_REAL",
+  apiKey: "AIzaSyAlJYIBiaTHu7Pkl9RZg8ia1mUVZ-5N4Sc",
   authDomain: "catering-2026.firebaseapp.com",
   projectId: "catering-2026",
   storageBucket: "catering-2026.appspot.com",
   messagingSenderId: "412063513316",
-  appId: "TU_APP_ID_REAL"
+  appId: "1:412063513316:web:1288220d612eacd8c64af1"
 };
 
+// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
+
+// Firestore
 export const db = getFirestore(app);
