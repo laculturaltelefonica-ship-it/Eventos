cc2c91caad73bb094c4d7caa468add992bbfe25a
laculturaltelefonica-ship-it <laculturaltelefonica@gmail.com>
2025-12-23 10:32:12 +0100

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,23 +1,16 @@
 //Eventos/firebase.js
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAlJYIBiaTHu7Pkl9RZg8ia1mUVZ-5N4Sc",
+  apiKey: "TU_API_KEY_REAL",
   authDomain: "catering-2026.firebaseapp.com",
   projectId: "catering-2026",
-  storageBucket: "catering-2026.firebasestorage.app",
+  storageBucket: "catering-2026.appspot.com",
   messagingSenderId: "412063513316",
-  appId: "1:412063513316:web:1288220d612eacd8c64af1",
-  measurementId: "G-8QHEZQWF3F"
+  appId: "TU_APP_ID_REAL"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
